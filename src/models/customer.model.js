c2d6dd87b75c4525d530b91daea80feafdd7b1b9
Mauro Gomes <mauroarturmauro@gmail.com>
2025-10-24 10:56:49 -0300

    feat: new ID generate for Customer

--- src/models/customer.model.js
+++ src/models/customer.model.js
@@ -1,11 +1,36 @@
 import mongoose from 'mongoose';
+import Counter from './counter.model.js';
 
 const CustomerSchema = new mongoose.Schema({
+  _id: { type: String, unique: true },
   name: { type: String, required: true },
   cpf: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   accounts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Account' }],
   consentData: { type: Boolean, default: false }
+}, {
+  _id: false
+});
+
+CustomerSchema.pre('save', async function(next) {
+  if (this.isNew) { 
+    try {
+      const counter = await Counter.findByIdAndUpdate(
+        { _id: 'customerId' },      
+        { $inc: { seq: 1 } },     
+        { new: true, upsert: true }                         
+      );
+
+      const newId = 'cus_' + String(counter.seq).padStart(3, '0');
+      
+      this._id = newId;
+      next();
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    next();
+  }
 });
 
 export default mongoose.model('Customer', CustomerSchema);
