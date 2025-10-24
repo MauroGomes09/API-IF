44108081900840526514c69ab98fc109e20f2a8d
Mauro Gomes <mauroarturmauro@gmail.com>
2025-10-24 11:19:46 -0300

    feat: add a new format to Account ID

--- src/models/account.model.js
+++ src/models/account.model.js
@@ -1,11 +1,35 @@
 import mongoose from 'mongoose';
 
 const AccountSchema = new mongoose.Schema({
+  _id: { type: String },
   type: { type: String, required: true, enum: ['checking', 'savings'] },
   branch: { type: String, required: true },
   number: { type: String, required: true, unique: true },
   balance: { type: Number, default: 0.00 },
   transactions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Transaction' }]
+}, {
+  _id: false
+});
+
+AccountSchema.pre('save', async function(next) {
+  if (this.isNew) {
+    try {
+      const counter = await Counter.findByIdAndUpdate(
+        { _id: 'accountId' },
+        { $inc: { seq: 1 } },
+        { new: true, upsert: true }
+      );
+
+      const newId = 'acc_' + String(counter.seq).padStart(3, '0');
+
+      this._id = newId; 
+      next();
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    next();
+  }
 });
 
 export default mongoose.model('Account', AccountSchema);
