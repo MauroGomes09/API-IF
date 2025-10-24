c9b9b57b1f733e6bcdc9964cc8ed6ce8803742fe
Mauro Gomes <mauroarturmauro@gmail.com>
2025-10-24 11:35:10 -0300

    feat: adjust the type of accounts in Schema

--- src/models/customer.model.js
+++ src/models/customer.model.js
@@ -6,7 +6,7 @@ const CustomerSchema = new mongoose.Schema({
   name: { type: String, required: true },
   cpf: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  accounts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Account' }],
+  accounts: [{ type: String, ref: 'Account' }],
   consentData: { type: Boolean, default: false }
 }, {
   _id: false
